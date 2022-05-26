fb05ab2ab0dbd494544cbd3b4766d3ad02a3c46c
Greg Wilson <gvwilson@third-bit.com>
2022-05-26 16:43:40 -0400

    Plate data

--- R/plates.R
+++ R/plates.R
@@ -7,7 +7,7 @@ LAYOUT_DIR <- str_interp("${DATA_DIR}/PLATE LAYOUTS FOR ELISA AND BCA MICROTILE 
 
 # Rename columns and rearrange into long format.
 rename_and_rearrange <- function(df, leading) {
-  colnames(df) <- c(leading, str_pad(seq(1, 24), 2, side="left", pad="0"))
+  colnames(df) <- c(all_of(leading), str_pad(seq(1, 24), 2, side="left", pad="0"))
   result <- df %>% pivot_longer(!leading, names_to="col", values_to="val")
   result
 }
@@ -42,10 +42,47 @@ read_elisa_layout <- function(which) {
 
 # Load all three for analysis and bind columns.
 read_all <- function(which) {
-  bca_readout <- read_bca_readout("1")
-  elisa_readout <- read_elisa_readout("1")
-  elisa_layout <- read_elisa_layout("1")
-  bind_cols(elisa_layout, elisa_readout["elisa"], bca_readout["bca"])  
+  bca_readout <- read_bca_readout(which)
+  elisa_readout <- read_elisa_readout(which)
+  elisa_layout <- read_elisa_layout(which)
+  bind_cols(elisa_layout, elisa_readout["elisa"], bca_readout["bca"])
 }
 
-View(read_all(""))
+ELISA_NORM <- c(
+  600, 600, 450, 450, 300, 300, 200, 200, 150, 150,
+  100, 100,  75,  75, 37.5, 37.5, 18.75, 18.75, 0, 0
+)
+
+# Create standard curve for ELISA.
+elisa_standard_curve <- function(df) {
+  # Get the values from row O.
+  # Discard columns 1, 2, 23, and 24.
+  values <- df %>%
+    filter(row == "O") %>%
+    filter(("02" < col) & (col < "23"))
+  # Line up with standard curve normalization values.
+  values <- bind_cols(values, elisa_norm=ELISA_NORM)
+  # Calculate simple linear fit.
+  result <- values %>% lm(formula = elisa ~ elisa_norm)
+  result$coefficients
+}
+
+BCA_NORM <- c(
+  2000, 2000, 1500, 1500, 1000, 1000, 750, 750,
+  500, 500, 250, 250, 125, 125, 62.5, 62.5,
+  31.25, 31.25, 0, 0
+)
+
+# Calculate standard curve for BCA.
+bca_standard_curve <- function(df) {
+  # Get the values from row P.
+  # Discard columns 1, 2, 23, and 24.
+  values <- df %>%
+    filter(row == "P") %>%
+    filter(("02" < col) & (col < "23"))
+  # Line up with standard curve normalization values.
+  values <- bind_cols(values, bca_norm=BCA_NORM)
+  # Calculate simple linear fit.
+  result <- values %>% lm(formula = elisa ~ bca_norm)
+  result$coefficients
+}
